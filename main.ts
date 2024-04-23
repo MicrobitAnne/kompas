471266ab590519303244f03a10693193f00956a2
MicrobitAnne <161485257+MicrobitAnne@users.noreply.github.com>
2024-04-23 16:06:22 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.forever(function () {
+    let kompas = 0
+    if (kompas >= 315 && kompas <= 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (kompas >= 45 && kompas <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (kompas >= 135 && kompas <= 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (kompas >= 225 && kompas <= 45) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
